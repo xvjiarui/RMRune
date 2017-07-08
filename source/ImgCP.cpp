d0ddb803eec4e971c122658c5205f668551e9e51
Jerry <xvjiarui0826@gmail.com>
2017-07-08 13:03:01 +0800

    real commit almost finish angle

--- source/ImgCP.cpp
+++ source/ImgCP.cpp
@@ -80,7 +80,7 @@ void ImgCP::ImageConsumer()
     const double ptz_camera_z = settings.gimbalSetting.GimbalZ;
     double theta = -atan((ptz_camera_y + barrel_ptz_offset_y)/overlap_dist);
     double r_data[] = {1,0,0,0,cos(theta),-sin(theta), 0, sin(theta), cos(theta)};
-    double t_data[] = {0, ptz_camera_y, ptz_camera_z}; // ptz org position in camera coodinate system
+    double t_data[] = {ptz_camera_x, ptz_camera_y, ptz_camera_z}; // ptz org position in camera coodinate system
     Mat t_camera_ptz(3,1, CV_64FC1, t_data);
     Mat r_camera_ptz(3,3, CV_64FC1, r_data);
     angleSolver.setRelationPoseCameraPTZ(r_camera_ptz, t_camera_ptz, barrel_ptz_offset_y);
